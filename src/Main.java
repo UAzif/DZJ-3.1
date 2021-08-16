6223699b15f8502fec1e4d9b7ceef09400496370
UAzif <begimot02@mail.ru>
2021-08-16 22:58:31 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 25_357;
+        int mil = ticketPrice/20;
+        System.out.println("Количество начисленных миль: " + mil);
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+
+        public static void main(String[] args) {
+            int ticketPrice = 25_357;
+            int milPrice = 35;
+            int mil = ticketPrice/milPrice;
+            System.out.println("Количество начисленных миль: " + mil);
+        }
+
+}
