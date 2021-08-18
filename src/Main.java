bc32c75130909671d2fcbda450790613f9cb3667
UAzif <begimot02@mail.ru>
2021-08-18 18:28:09 +0500

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 25_357;
-        int mil = ticketPrice/20;
+        int mil = ticketPrice / 20;
         System.out.println("Количество начисленных миль: " + mil);
     }
 }
